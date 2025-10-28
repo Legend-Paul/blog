03e0757cd6d14b753ff6aadd080b686c9bedee9e
Legend Paul <qamunyamaina950@gmail.com>
2025-10-28 13:56:16 +0300

    Create admin dashboard route

---
+++ frontend-admin/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Dashboard from "../pages/Dashboard/Dashboard";
+
+export default function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Router>
+    );
+}
