d2e62e54f6e916e976ebcce71ee8824731baf0d6
Legend Paul <qamunyamaina950@gmail.com>
2025-10-28 10:41:20 +0300

    Include blog likes when gettin a blog

--- Backend/controllers/adminBlogHandler.js
+++ Backend/controllers/adminBlogHandler.js
@@ -42,17 +42,38 @@ export const getBlogs = async (req, res) => {
 };
 
 // get a blog
+// get a blog
 export const getBlog = async (req, res) => {
     try {
         const { slug } = req.params;
-        const blog = await prisma.blog.findMany({
-            where: { slug, authorId: req.user.id },
+        const blog = await prisma.blog.findUnique({
+            where: { slug },
+            include: {
+                _count: {
+                    select: {
+                        likes: true,
+                    },
+                },
+                author: {
+                    select: {
+                        id: true,
+                        fullName: true,
+                        username: true,
+                        role: true,
+                    },
+                },
+            },
         });
+
+        if (!blog) {
+            return res.status(404).json({ message: "Blog not found" });
+        }
 
         return res
             .status(200)
-            .json({ message: "Blog accessed successifully", data: blog });
+            .json({ message: "Blog accessed successfully", data: blog });
     } catch (err) {
+        console.error("Error fetching blog:", err);
         return res.status(500).json({ message: "Server error" });
     }
 };
