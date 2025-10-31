c3af27921ce3553ec94a77650a0916d541c5afc1
Legend-Paul <qamunyamaina950@gmail.com>
2025-10-31 22:30:18 +0300

    Display tinymce editor

--- frontend-admin/src/pages/NewBlog/NewBlog.jsx
+++ frontend-admin/src/pages/NewBlog/NewBlog.jsx
@@ -1,11 +1,30 @@
-import styles from "./NewBlog.modules.css";
+// import styles from "./NewBlog.modules.css";
+import { useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 import Header from "../../components/Header/Header";
 
 export default function NewBlog() {
+  const [content, setContent] = useState(null);
+
   return (
     <div className="new-blog-container">
       <Header />
-      <section className="new-blog"></section>
+      <section className="new-blog">
+        <div>
+          <Editor
+            apiKey="your-api-key-here"
+            value={content}
+            onEditorChange={(newContent) => setContent(newContent)}
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: ["advlist", "autolink", "lists", "link", "image"],
+              toolbar: "undo redo | bold italic | bullist numlist",
+            }}
+          />
+          <button onClick={() => console.log(content)}>Get Content</button>
+        </div>
+      </section>
     </div>
   );
 }
