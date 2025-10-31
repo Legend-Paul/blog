5fa628279998aa5759935cce1088223fcedff162
Legend-Paul <qamunyamaina950@gmail.com>
2025-10-31 14:06:57 +0300

    Style active nav

--- frontend-admin/src/components/Header/Header.jsx
+++ frontend-admin/src/components/Header/Header.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import styles from "./Header.module.css";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+  const [active, setActive] = useState("dashboard");
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
     <div className={styles["header-container"]}>
       <header>
@@ -10,17 +15,45 @@ export default function Header() {
         <nav>
           <ul>
             <li>
-              <a href="/dashboard">Dashboard</a>
+              <a
+                className={
+                  styles[
+                    `${
+                      currentPath.includes("dashboard") ||
+                      currentPath.endsWith("/")
+                        ? "active"
+                        : ""
+                    }`
+                  ]
+                }
+                href="/dashboard"
+              >
+                Dashboard
+              </a>
             </li>
           </ul>
           <ul>
             <li>
-              <a href="/">Blogs</a>
+              <a
+                className={
+                  styles[`${currentPath.includes("blog") ? "active" : ""}`]
+                }
+                href="/"
+              >
+                Blogs
+              </a>
             </li>
           </ul>
           <ul>
             <li>
-              <a href="/signout">Sign Out</a>
+              <a
+                className={
+                  styles[`${currentPath.includes("signout") ? "active" : ""}`]
+                }
+                href="/signout"
+              >
+                Sign Out
+              </a>
             </li>
           </ul>
         </nav>
