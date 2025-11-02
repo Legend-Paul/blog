e480a87d83d6a506acf004b8fb645c949197df12
Legend-Paul <qamunyamaina950@gmail.com>
2025-11-02 03:37:13 +0300

    Create error page

--- frontend-admin/src/App.jsx
+++ frontend-admin/src/App.jsx
@@ -8,24 +8,27 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import Blog from "./pages/Blogs/Blogs";
 import NewBlog, { Action as newBlogAction } from "./pages/NewBlog/NewBlog";
 import PreviewBlog from "./pages/PreviewBlog/PreviewBlog";
+import ErrorPage from "./Error/ErrorPage";
 import "./index.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/api/blogs" element={<Blog />} />
-      <Route
-        path="/api/blogs/new"
-        element={<NewBlog />}
-        action={newBlogAction}
-      />
-      <Route
-        path="/api/blog/:slug"
-        element={<PreviewBlog />}
-        action={newBlogAction}
-      />
+      <Route errorElement={<ErrorPage />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/api/blogs" element={<Blog />} />
+        <Route
+          path="/api/blogs/new"
+          element={<NewBlog />}
+          action={newBlogAction}
+        />
+        <Route
+          path="/api/blog/:slug"
+          element={<PreviewBlog />}
+          action={newBlogAction}
+        />
+      </Route>
     </>
   )
 );
