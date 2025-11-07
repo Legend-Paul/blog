32bd8ce90ec34109e1dc9b23c2bef39459a8cdbb
Legend-Paul <qamunyamaina950@gmail.com>
2025-11-07 12:24:25 +0300

    Add like and reply button to the comment

--- frontend-public/src/pages/Blog/Blog.jsx
+++ frontend-public/src/pages/Blog/Blog.jsx
@@ -98,7 +98,40 @@ function Comments({ comments, formatDate }) {
                   <span>{formatDate(comment.createdAt)}</span>
                 </div>
                 <p className={styles["comment-text"]}>{comment.content}</p>
-                <div className={styles["comment-icons"]}></div>
+                <div className={styles["comment-icons"]}>
+                  <button className={styles["comment-icon"]}>
+                    <svg
+                      className={styles["nav-icon"]}
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+                      />
+                    </svg>
+                    <span>Likes {comment._count.likes}</span>
+                  </button>
+                  <button className={styles["comment-icon"]}>
+                    <svg
+                      className={styles["nav-icon"]}
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6M9 10h.01"
+                      />
+                    </svg>
+                    <span>Replies {comment._count.replies}</span>
+                  </button>
+                </div>
               </div>
               <div className={styles["replies"]}>
                 {comment.replies.length > 0 ? (
