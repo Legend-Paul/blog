09086ac49c6eb76eb50e6919e8668564413a43a6
Legend-Paul <qamunyamaina950@gmail.com>
2025-11-07 21:58:17 +0300

    Add like to  the blog

--- frontend-public/src/pages/Blog/Blog.jsx
+++ frontend-public/src/pages/Blog/Blog.jsx
@@ -153,7 +153,24 @@ export default function Blog() {
       <main>
         <div dangerouslySetInnerHTML={{ __html: blog.content }} />
         <OtherBlogs blogs={blogs} author={author} formatDate={formatDate} />
-
+        <div className={styles["blog-likes"]}>
+          <button>
+            <svg
+              className="nav-icon"
+              fill="currentColor"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>{" "}
+            Likes {blog._count.likes}
+          </button>
+        </div>
         <div id="comments" className="comments-section-container">
           <div className={styles["comments-heading"]}>
             <h3>
