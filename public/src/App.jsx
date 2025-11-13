ed23ec052915e0cbce80140fac2e70398debbc07
Legend-Paul <qamunyamaina950@gmail.com>
2025-11-13 16:57:42 +0300

    Create a sepperate ?:author route

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -24,6 +24,7 @@ function AuthorLayout() {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+      <Route path="/:author" element={<Blogs />} />
       <Route
         path="/:author"
         errorElement={<ErrorPage />}
